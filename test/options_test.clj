f54afb991f489593453db9047d1f1265ec3fa394
Ben Oliver <benjamin.e.oliver@gmail.com>
2018-08-19 13:02:07 +0200

    Options

---
+++ test/options_test.clj
@@ -0,0 +1,28 @@
+(ns options-test
+  (:require [clojure.test :refer :all]
+            [clj-arangodb.options :as options])
+  (:import [com.arangodb.model
+            CollectionCreateOptions]))
+
+(deftest build-test
+  (testing "Can pass args to variadic function using a String Array"
+    (is (= CollectionCreateOptions
+           (->> {:doCompact true :shardKeys (into-array ["a" "b" "c" "d"])}
+                (options/build CollectionCreateOptions)
+                class))))
+  (testing "Can call multiple times - arguments evaluated from left to right"
+    (is (= true
+           (->> [[:doCompact true]]
+                ^CollectionCreateOptions (options/build CollectionCreateOptions)
+                .getDoCompact)))
+    (is (= false
+           (->> [[:doCompact true] [:doCompact false]]
+                ^CollectionCreateOptions (options/build CollectionCreateOptions)
+                .getDoCompact)))
+    (is (= true
+           (->> [[:doCompact true] [:doCompact false] [:doCompact true]]
+                ^CollectionCreateOptions (options/build CollectionCreateOptions)
+                .getDoCompact))))
+  (testing "passing a collection object returns the object"
+    (let [o (CollectionCreateOptions.)]
+      (is (= o (options/build CollectionCreateOptions o))))))
